03ce39ff41b5a4634f4fd1f9a8f634e5026129c4
andrei_dzimchuk_cp <SND\andrei_dzimchuk_cp>
2011-10-26 15:34:59 +0000

    small rearrangements

--- Development/Andrei/Pvp.Core.WindowsForms/MediaEngine/Core/MediaWindow.cs
+++ Development/Andrei/Pvp.Core.WindowsForms/MediaEngine/Core/MediaWindow.cs
@@ -26,21 +26,80 @@ namespace Dzimchuk.MediaEngine.Core
 
         #region Imported functions from nwnd.dll
 
-        [DllImport("nwnd.dll")]
-        public static extern IntPtr CreateMediaWindow(IntPtr hParent, int nWidth, int nHeight);
+        private static class NwndWrapper
+        {
+            [DllImport("nwnd.dll")]
+            public static extern IntPtr CreateMediaWindow(IntPtr hParent, int nWidth, int nHeight);
 
-        [DllImport("nwnd.dll")]
-        public static extern void SetRunning([MarshalAs(UnmanagedType.Bool)] bool bRunning,
-            IVMRWindowlessControl pVMR, IVMRWindowlessControl9 pVMR9, IMFVideoDisplayControl pEVR);
+            [DllImport("nwnd.dll")]
+            public static extern void SetRunning([MarshalAs(UnmanagedType.Bool)] bool bRunning,
+                IVMRWindowlessControl pVMR, IVMRWindowlessControl9 pVMR9, IMFVideoDisplayControl pEVR);
 
-        [DllImport("nwnd.dll")]
-        public static extern void SetLogo(IntPtr hLogo);
+            [DllImport("nwnd.dll")]
+            public static extern void SetLogo(IntPtr hLogo);
 
-        [DllImport("nwnd.dll")]
-        public static extern void IsShowLogo([MarshalAs(UnmanagedType.Bool)] bool bShow);
+            [DllImport("nwnd.dll")]
+            public static extern void IsShowLogo([MarshalAs(UnmanagedType.Bool)] bool bShow);
 
-        [DllImport("nwnd.dll")]
-        public static extern void InvalidateMediaWindow();
+            [DllImport("nwnd.dll")]
+            public static extern void InvalidateMediaWindow();
+        }
+
+        private static class Nwnd64Wrapper
+        {
+            [DllImport("nwnd64.dll")]
+            public static extern IntPtr CreateMediaWindow(IntPtr hParent, int nWidth, int nHeight);
+
+            [DllImport("nwnd64.dll")]
+            public static extern void SetRunning([MarshalAs(UnmanagedType.Bool)] bool bRunning,
+                IVMRWindowlessControl pVMR, IVMRWindowlessControl9 pVMR9, IMFVideoDisplayControl pEVR);
+
+            [DllImport("nwnd64.dll")]
+            public static extern void SetLogo(IntPtr hLogo);
+
+            [DllImport("nwnd64.dll")]
+            public static extern void IsShowLogo([MarshalAs(UnmanagedType.Bool)] bool bShow);
+
+            [DllImport("nwnd64.dll")]
+            public static extern void InvalidateMediaWindow();
+        }
+
+        private static IntPtr CreateMediaWindow(IntPtr hParent, int nWidth, int nHeight)
+        {
+            return IntPtr.Size == 8 ? Nwnd64Wrapper.CreateMediaWindow(hParent, nWidth, nHeight) : NwndWrapper.CreateMediaWindow(hParent, nWidth, nHeight);
+        }
+
+        private static void SetRunning(bool bRunning, IVMRWindowlessControl pVMR, IVMRWindowlessControl9 pVMR9, IMFVideoDisplayControl pEVR)
+        {
+            if (IntPtr.Size == 8)
+                Nwnd64Wrapper.SetRunning(bRunning, pVMR, pVMR9, pEVR);
+            else
+                NwndWrapper.SetRunning(bRunning, pVMR, pVMR9, pEVR);
+        }
+
+        public static void SetLogo(IntPtr hLogo)
+        {
+            if (IntPtr.Size == 8)
+                Nwnd64Wrapper.SetLogo(hLogo);
+            else
+                NwndWrapper.SetLogo(hLogo);
+        }
+
+        public static void IsShowLogo(bool bShow)
+        {
+            if (IntPtr.Size == 8)
+                Nwnd64Wrapper.IsShowLogo(bShow);
+            else
+                NwndWrapper.IsShowLogo(bShow);
+        }
+
+        public static void InvalidateMediaWindow()
+        {
+            if (IntPtr.Size == 8)
+                Nwnd64Wrapper.InvalidateMediaWindow();
+            else
+                NwndWrapper.InvalidateMediaWindow();
+        }
 
         #endregion
         
