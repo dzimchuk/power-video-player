8c738004bb9063bf4207e8b986d89de718cd6666
Andrei Dzimchuk <3907708+dzimchuk@users.noreply.github.com>
2012-11-12 19:30:50 +0000

    better mouse cursor handling

--- Players/WPF/Pvp.App/Service/CursorManager.cs
+++ Players/WPF/Pvp.App/Service/CursorManager.cs
@@ -8,7 +8,6 @@ namespace Pvp.App.Service
     internal class CursorManager : ICursorManager
     {
         private IntPtr _previousCursor = IntPtr.Zero;
-        private bool _isCursorVisible = true;
 
         public void ShowCursor()
         {
@@ -16,22 +15,20 @@ namespace Pvp.App.Service
             {
                 WindowsManagement.SetCursor(_previousCursor);
             }
-
-            _isCursorVisible = true;
         }
 
         public void HideCursor()
         {
             var previousCursor = WindowsManagement.SetCursor(IntPtr.Zero);
             if (previousCursor != IntPtr.Zero)
+            {
                 _previousCursor = previousCursor;
-
-            _isCursorVisible = false;
+            }
         }
 
         public bool IsCursorVisible
         {
-            get { return _isCursorVisible; }
+            get { return WindowsManagement.GetCursor() != IntPtr.Zero; }
         }
     }
 }
